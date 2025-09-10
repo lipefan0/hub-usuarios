b07cd6459dd2e2b18acf895a3917b257cbe92c2c
Felipe Oliveira <lipefan0@gmail.com>
2025-09-10 09:43:05 -0300

    commit inicial

---
+++ src/main/java/br/com/upvisibility/hub_usuarios/HubUsuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.upvisibility.hub_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/upvisibility/hub_usuarios/HubUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upvisibility.hub_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
