8b6c6b502ef708658d93a9c7242cd14951b4003f
Felipe Oliveira <lipefan0@gmail.com>
2025-09-10 10:25:45 -0300

    adiciona entity e repositories de usuario e endereco

---
+++ src/main/java/br/com/upvisibility/hub_usuarios/infra/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package br.com.upvisibility.hub_usuarios.infra.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "db_usuarios")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 255)
+    private String nome;
+    @Column(name = "email_usuario", unique = true)
+    private String email;
+    private String senha;
+    private String celular;
+    @Column(name = "numero_documento",  unique = true)
+    private String numeroDocumento;
+    @Column(name = "tipo_pessoa")
+    private String tipoPessoa;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<EnderecoEntity> enderecos;
+}
